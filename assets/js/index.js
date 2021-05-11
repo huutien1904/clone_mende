20bb48f73990a066c5feb33c59a91535a7606fe8
Huu Tien <huutien1904.com>
2021-05-11 15:48:52 +0700

    slider show

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,67 @@
-// let modelItem = document.getElementsByClassName("section_item");
-// console.log(modelItem);
-// Array.from(modelItem).forEach((item, index) => {
-//   item.addEventListener("click", function () {
-//     item.children[0].classList.toggle("link-active");
-//     console.log(item.children);
-//     let currMenu = document.querySelector(".link-active");
-
-//     if (currMenu) {
-//       currMenu.classList.remove("link-active");
-//     }
-//   });
+$(document).ready(function () {
+  // toggle button
+  $(".section_item").click(function () {
+    $(this).find(".model-item").toggleClass("link-active");
+    $(this).siblings().find(".model-item").removeClass("link-active");
+  });
+});
+// $(".menu-show").hover(function () {
+//   if ($(this).hasClass("expland")) {
+//     $(".menu-item_list").addClass("expland");
+//     $(".menu-item_list").slideDown();
+//     $(".menu-show").mouseout(function () {
+//       $(".menu-item_list").slideUp();
+//     });
+//   } else {
+//     $(".menu-item_list").slideUp();
+//     $(".menu-item_list").addClass("expland");
+//   }
 // });
-console.log($(".section_item"));
-$(".section_item").click(function () {
-  $(this).find(".model-item").toggleClass("link-active");
-  $(this).siblings().find(".model-item").removeClass("link-active");
+
+// $(".menu-show").hover(function () {
+//   $(".menu-item_list").css("display", "block");
+//   $(".menu-item_list").slideDown();
+// });
+// !$(".menu-show").mouseout(function () {
+//   $(".menu-item_list").slideUp();
+// });
+// !$(".menu-item_list").mouseout(function () {
+//   $(".menu-item_list").slideUp();
+// });
+
+// slider
+var slideContainer = document.querySelector(".slider");
+var sliderImageAll = document.querySelectorAll(".slider img");
+
+var prevBtn = document.querySelector("#prevBtn");
+var nextBtn = document.querySelector("#nextBtn");
+
+let counter = 1;
+const size = sliderImageAll[0].clientWidth;
+slideContainer.style.transform = "translateX(" + -size * counter + "px)";
+
+nextBtn.addEventListener("click", () => {
+  if (counter >= sliderImageAll.length - 1) return;
+  counter++;
+
+  slideContainer.style.transform = "translateX(" + -size * counter + "px)";
+  slideContainer.style.transition = "transform 1s ease-in-out";
+});
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+  counter--;
+  slideContainer.style.transform = "translateX(" + -size * counter + "px)";
+  slideContainer.style.transition = "transform 0.4s ease-in-out";
+});
+slideContainer.addEventListener("transitionend", () => {
+  if (sliderImageAll[counter].id === "lastclone") {
+    slideContainer.style.transition = "none";
+    counter = sliderImageAll.length - 2;
+    slideContainer.style.transform = "translateX(" + -size * counter + "px)";
+  }
+  if (sliderImageAll[counter].id === "firstclone") {
+    slideContainer.style.transition = "none";
+    counter = sliderImageAll.length - counter;
+    slideContainer.style.transform = "translateX(" + -size * counter + "px)";
+  }
 });
